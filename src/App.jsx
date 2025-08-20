4bfb9cfdc10f72a13e28834dacd94f9f387beabf
joseeduardopompeumarques0202-alt <joseeduardopompeumarques0202@gmail.com>
2025-08-20 16:04:32 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import React from "react";
+
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-pink-50">
+      <h1 className="text-4xl font-bold text-pink-500">
+        Bem-vindo ao Doce Encanto!
+      </h1>
+    </div>
+  );
+}
+
+export default App;
