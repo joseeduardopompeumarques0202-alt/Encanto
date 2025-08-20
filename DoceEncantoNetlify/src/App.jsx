c35090c1b16f71ccb4433262ff50a1aedce2d19c
joseeduardopompeumarques0202-alt <joseeduardopompeumarques0202@gmail.com>
2025-08-20 15:44:41 -0300

    Delete DoceEncantoNetlify directory

--- DoceEncantoNetlify/src/App.jsx
+++
@@ -1,6 +0,0 @@
-
-import React, { useMemo, useState } from "react";
-// --- Cole aqui todo o código React que você enviou inicialmente ---
-export default function App() {
-  return <div>Seu site Doce Encanto React aqui!</div>;
-}
