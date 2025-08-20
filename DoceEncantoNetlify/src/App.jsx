c3161ba4e5b438d7042f10c8749a9dbedb2f334e
joseeduardopompeumarques0202-alt <joseeduardopompeumarques0202@gmail.com>
2025-08-20 15:22:03 -0300

    Add files via upload

---
+++ DoceEncantoNetlify/src/App.jsx
@@ -0,0 +1,6 @@
+
+import React, { useMemo, useState } from "react";
+// --- Cole aqui todo o código React que você enviou inicialmente ---
+export default function App() {
+  return <div>Seu site Doce Encanto React aqui!</div>;
+}
